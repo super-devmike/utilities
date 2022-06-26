f3fe016b05a08c96755ae23663163649979ec185
Michael E Adebisi <michaeladebisi@punch.cool>
2022-06-26 19:53:26 +0100

    added example plist and docstring

--- apps_on_startup/apps_on_startup.py
+++ apps_on_startup/apps_on_startup.py
@@ -1,6 +1,20 @@
 '''
 This python script open some apps on start.
 The apps are defined in the list 'apps_to_open'.
+
+METHODS OF LOADING
+
+PLIST
+1. Create a plist file similar to example_plist in the folder apps_on_startup.
+2. Add the plist file to the list of plist files in ~/Library/LaunchAgents
+3. launchctl load /path/to/plist/file
+
+SHELL SCRIPT
+1. Create a shell script in the folder apps_on_startup.
+2. Example 
+`#!/bin/bash
+python3 /absolute/path/to/pyfile`
+3. add the .sh file to "System Preference -> Users and Groups -> Login items", the .sh script will call the python file.
 '''
 
 import os
--- apps_on_startup/shell.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/zsh
-
-
-/usr/local/bin/python3 /Users/michael/Documents/devmike/python/projects/apps_on_startup/apps_on_startup.py
